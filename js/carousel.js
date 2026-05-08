332e3c332eac3c5f66587f839519ed65189d73e6
Andreik <andreikelsarle@gmail.com>
2026-05-08 11:41:14 -0300

    melhorando fluidez do carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
-//Array storage class
+// Array storage class
 let carouselArr = [];
 
-//class Carousel
+// class Carousel
 class Carousel {
   constructor(image, title, url) {
     this.image = image;
@@ -10,48 +10,83 @@ class Carousel {
   }
 
   static Start(arr) {
-    if (arr) {
-      if (arr.length > 0) {
-        Carousel._sequence = 0;
-        Carousel._size = arr.length;
-        Carousel._firstLoad = true; 
-
-        Carousel.Next();
-
-        Carousel._interval = setInterval(function () {
-          Carousel.Next();
-        }, 3000);
-      }
-    } else {
+    if (!arr || arr.length === 0) {
       throw "Method Start need a Array Variable.";
     }
+
+    Carousel._sequence = 0;
+    Carousel._size = arr.length;
+    Carousel._firstLoad = true;
+
+    Carousel.Next();
+
+    // dots click (✔ ADICIONADO)
+    const dots = document.querySelectorAll(".dot");
+    dots.forEach((dot) => {
+      dot.addEventListener("click", () => {
+        Carousel.GoTo(Number(dot.dataset.index));
+      });
+    });
+
+    Carousel._interval = setInterval(function () {
+      Carousel.Next();
+    }, 2000);
   }
 
-  static Next() {
+  // ✔ NOVO: ir direto pro slide
+  static GoTo(index) {
+    Carousel._sequence = index;
+    Carousel._firstLoad = false;
+    Carousel.Next(true);
+  }
+
+  static Next(skipAuto = false) {
     const slide = carouselArr[Carousel._sequence];
 
     const image = document.getElementById("carousel");
     const title = document.getElementById("carousel-title");
     const link = document.getElementById("carousel-link");
+    const container = image.closest(".imagem-home");
 
- 
     if (Carousel._firstLoad) {
-      image.src = slide.image;
-      title.innerHTML = slide.title;
-      link.href = slide.url;
 
+      if (Carousel._sequence === 0) {
+        title.innerHTML = `${slide.title} Verifique novidades <a href="lancamento.html">aqui.</a>`;
+      } else {
+        title.innerHTML = slide.title;
+      }
+
+      image.src = slide.image;
+      link.href = slide.url;
       Carousel._firstLoad = false;
+
     } else {
-     
-      image.style.opacity = "0.60";
+      container.style.backgroundImage = `url(${image.src})`;
+      image.style.opacity = "0";
 
       setTimeout(() => {
+
+        if (Carousel._sequence === 1) {
+          title.innerHTML = `Esta é a nova Ford Ranger 2022. Verifique novidades <a href="lancamento.html">aqui.</a>`;
+        } else {
+          title.innerHTML = slide.title;
+        }
+
         image.src = slide.image;
-        title.innerHTML = slide.title;
         link.href = slide.url;
 
-        image.style.opacity = "1";
-      }, 400);
+        setTimeout(() => {
+          image.style.opacity = "1";
+        }, 50);
+
+      }, 250);
+    }
+
+    // ✔ UPDATE DOTS (ADICIONADO)
+    const dots = document.querySelectorAll(".dot");
+    dots.forEach((d) => d.classList.remove("active"));
+    if (dots[Carousel._sequence]) {
+      dots[Carousel._sequence].classList.add("active");
     }
 
     Carousel._sequence++;
@@ -64,27 +99,15 @@ class Carousel {
 
 // Slides
 carouselArr.push(
-  new Carousel(
-    "img/imagem_1.jpg",
-    "Essa é a nova Ford Ranger 2022.",
-    "lancamento.html"
-  )
+  new Carousel("img/imagem_1.jpg", "Esta é a nova Ranger Ford 2022.", "lancamento.html")
 );
 
 carouselArr.push(
-  new Carousel(
-    "img/imagem_2.jpg",
-    "Essa é a nova Ford Ranger 2022.",
-    "lancamento.html"
-  )
+  new Carousel("img/imagem_2.jpg", "Ford: conheça a nossa história.", "#")
 );
 
 carouselArr.push(
-  new Carousel(
-    "img/imagem_3.jpg",
-    "Essa é a nova Ford Ranger 2022.",
-    "lancamento.html"
-  )
+  new Carousel("img/imagem_3.jpg", "Nova Ford Bronco Sport 2022.", "#")
 );
 
 // Inicio
